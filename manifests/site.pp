1bc4c748d05abd201383349a8521e9e0862cb6a8
Saurabh D <saurabh.dikshit@puppet.com>
2023-09-08 12:17:10 +0100

    checking dsc3

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,3 +38,14 @@ class { 'docker':
   version => latest,
 }
 }
+
+node 'sdwind2019.c.customer-support-scratchpad.internal' {
+ dsc_scheduledtask { 'SessionPopup' :
+  dsc_taskname => 'SessionPopup',
+  dsc_actionexecutable => powershell,
+  dsc_actionarguments =>'-File "C:\Program Files\windowspowershell\scripts\Epic.Wss.sessionpopup\Epic.Wss.SessionPopup.ps1"',
+  dsc_scheduletype => 'atlogon',
+  dsc_enable => $enable,
+  dsc_ensure => $ensure,
+ }
+}
