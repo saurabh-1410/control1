9d02eda97cfd303c2892162e563384a85b9192a0
Saurabh D <saurabh.dikshit@puppet.com>
2024-03-25 10:52:56 +0000

    profile1

--- manifests/site.pp
+++ manifests/site.pp
@@ -33,23 +33,23 @@ node default {
 #    include role::agent
 #  }
 }
-node 'sdclnt-cec56b-0.us-west1-c.c.customer-support-scratchpad.internal' {
-class { 'docker':
-  version => latest,
-}
-}
+#node 'sdclnt-cec56b-0.us-west1-c.c.customer-support-scratchpad.internal' {
+#class { 'docker':
+#  version => latest,
+#}
+#}
+#
+#node 'sdwind2019.c.customer-support-scratchpad.internal' {
+# dsc_scheduledtask { 'SessionPopup' :
+#  dsc_taskname => 'SessionPopup',
+#  dsc_actionexecutable => powershell,
+#  dsc_actionarguments =>'-File "C:\Program Files\windowspowershell\scripts\Epic.Wss.sessionpopup\Epic.Wss.SessionPopup.ps1"',
+#  dsc_scheduletype => 'AtLogOn',
+#  dsc_enable => $enable,
+#  dsc_ensure => $ensure,
+# }
+#}
 
-node 'sdwind2019.c.customer-support-scratchpad.internal' {
- dsc_scheduledtask { 'SessionPopup' :
-  dsc_taskname => 'SessionPopup',
-  dsc_actionexecutable => powershell,
-  dsc_actionarguments =>'-File "C:\Program Files\windowspowershell\scripts\Epic.Wss.sessionpopup\Epic.Wss.SessionPopup.ps1"',
-  dsc_scheduletype => 'AtLogOn',
-  dsc_enable => $enable,
-  dsc_ensure => $ensure,
- }
-}
-
-node 'sdinframas.c.customer-support-scratchpad.internal' {
-  include pe_status_check
-}
+#node 'sdinframas.c.customer-support-scratchpad.internal' {
+#  include pe_status_check
+#}
